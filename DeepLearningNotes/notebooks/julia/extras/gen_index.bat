c1f321bc797666d6ae404e1666ef648dca91cd81
hugh.murrell <hugh.murrell@gmail.com>
2019-08-10 11:44:35 +0200

    html update

--- DeepLearningNotes/notebooks/julia/extras/gen_index.bat
+++ DeepLearningNotes/notebooks/julia/extras/gen_index.bat
@@ -1,1 +1,1 @@
-tree -H '.' -T "Jupyter notebooks using Julia and Flux <br> Deep Learning Notes <br> (use right click and save to download)" -FL 1 --noreport --charset utf-8 --matchdirs -P "*.ipynb" | grep -v extras > index.html
+tree -H '.' -T "Jupyter notebooks <br> using Julia and Flux <br> with Deep Learning Notes <br> (use right click to download)" -FL 1 --noreport --charset utf-8 --matchdirs -P "*.ipynb" | grep -v extras > index.html
