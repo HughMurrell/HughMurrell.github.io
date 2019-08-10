b6416cd8b3264acbfa37b88a699e4d12f251d1f5
hugh.murrell <hugh.murrell@gmail.com>
2019-08-10 10:35:52 +0200

    added deep.pdf (v1)

--- DeepLearningNotes/notebooks/julia/extras/gen_index.bat
+++ DeepLearningNotes/notebooks/julia/extras/gen_index.bat
@@ -1,1 +1,1 @@
-tree -H '.' -FL 1 --noreport --charset utf-8 --matchdirs -P "*.ipynb" | grep -v extras > index.html
+tree -H '.' -T "Jupyter notebooks <br> (use right click and save to download)" -FL 1 --noreport --charset utf-8 --matchdirs -P "*.ipynb" | grep -v extras > index.html
